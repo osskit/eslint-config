9c38a3b8a9f5eaeee42cbe106b370232c25aa00b
Eden Turgeman <edmx0.et@gmail.com>
2022-12-03 13:39:14 +0200

    Update react.js

--- fixtures/react.js
+++ fixtures/react.js
@@ -1,3 +1,3 @@
 import React from 'react'; // eslint-disable-line import/no-extraneous-dependencies
 
-export default () => <span aria-label='lol'>{'lol'}</span>;
+export default () => <span aria-label={'lol'}>{'lol'}</span>;
