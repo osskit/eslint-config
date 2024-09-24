783559121a2831dc3e268950f36bb4fa28083010
Noam Okman <noamokman@gmail.com>
2024-09-24 13:30:54 +0300

    fix react hooks plugin (#49)

--- sampleConfigs/react-test.config.js
+++ sampleConfigs/react-test.config.js
@@ -1,3 +1,26 @@
 import { baseConfig, reactConfig, testConfig } from '../src/index.js';
 
-export default [baseConfig, reactConfig, testConfig];
+export default [
+  {
+    ...baseConfig,
+    languageOptions: {
+      ...baseConfig.languageOptions,
+      parserOptions: {
+        ...baseConfig.languageOptions.parserOptions,
+        ecmaFeatures: {
+          ...baseConfig.languageOptions.parserOptions.ecmaFeatures,
+          jsx: true,
+        },
+      },
+    },
+    files: ['fixtures/*.js', 'fixtures/*.ts', 'fixtures/*.tsx'],
+  },
+  {
+    ...reactConfig,
+    files: ['fixtures/*.js', 'fixtures/*.ts', 'fixtures/*.tsx'],
+  },
+  {
+    ...testConfig,
+    files: ['fixtures/*.spec.js', 'fixtures/*.spec.ts', 'fixtures/*.spec.tsx'],
+  },
+];
